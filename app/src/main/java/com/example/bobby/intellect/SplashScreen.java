5ee8ba72def90a15c3689caafd6a198f6d587822
pbsarathy21 <43232117+pbsarathy21@users.noreply.github.com>
2018-10-19 11:53:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bobby/intellect/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.bobby.intellect;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMEOUT);
+
+
+    }
+}
